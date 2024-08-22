682e8b4e94873e48fe03660084f9273dd61c3d0b
surepos <125876053+surepos@users.noreply.github.com>
2024-08-22 17:08:17 +0900

    now

--- src/App.js
+++ src/App.js
@@ -503,77 +503,15 @@ function App() {
       </motion.div>
     )
   ) : (
-    <div className="App">
     <motion.div
+      key={playerSide}
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.5, delay: 0.5 }}
-      className="gameMode">
-      <div className="gameMode">
-        <input type="checkbox" name="gameMode" id="gameMode" checked={isHardMode} onChange={handleCheckboxChange}/>
-        <label htmlFor="gameMode" >
-        <svg className='ai' fill="#000000" width="800px" height="800px" viewBox="0 0 35 35" data-name="Layer 2" id="e73e2821-510d-456e-b3bd-9363037e93e3" xmlns="http://www.w3.org/2000/svg"><path d="M11.933,15.055H3.479A3.232,3.232,0,0,1,.25,11.827V3.478A3.232,3.232,0,0,1,3.479.25h8.454a3.232,3.232,0,0,1,3.228,3.228v8.349A3.232,3.232,0,0,1,11.933,15.055ZM3.479,2.75a.73.73,0,0,0-.729.728v8.349a.73.73,0,0,0,.729.728h8.454a.729.729,0,0,0,.728-.728V3.478a.729.729,0,0,0-.728-.728Z"/><path d="M11.974,34.75H3.52A3.233,3.233,0,0,1,.291,31.521V23.173A3.232,3.232,0,0,1,3.52,19.945h8.454A3.232,3.232,0,0,1,15.2,23.173v8.348A3.232,3.232,0,0,1,11.974,34.75ZM3.52,22.445a.73.73,0,0,0-.729.728v8.348a.73.73,0,0,0,.729.729h8.454a.73.73,0,0,0,.728-.729V23.173a.729.729,0,0,0-.728-.728Z"/><path d="M31.522,34.75H23.068a3.233,3.233,0,0,1-3.229-3.229V23.173a3.232,3.232,0,0,1,3.229-3.228h8.454a3.232,3.232,0,0,1,3.228,3.228v8.348A3.232,3.232,0,0,1,31.522,34.75Zm-8.454-12.3a.73.73,0,0,0-.729.728v8.348a.73.73,0,0,0,.729.729h8.454a.73.73,0,0,0,.728-.729V23.173a.729.729,0,0,0-.728-.728Z"/><path d="M27.3,15.055a7.4,7.4,0,1,1,7.455-7.4A7.437,7.437,0,0,1,27.3,15.055Zm0-12.3a4.9,4.9,0,1,0,4.955,4.9A4.935,4.935,0,0,0,27.3,2.75Z"/></svg>
-          <svg
-            className="multiplayer"
-            width="800px"
-            height="800px"
-            viewBox="0 0 24 24"
-            fill="#000000"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M14 2C14 2.74028 13.5978 3.38663 13 3.73244V4H20C21.6569 4 23 5.34315 23 7V19C23 20.6569 21.6569 22 20 22H4C2.34315 22 1 20.6569 1 19V7C1 5.34315 2.34315 4 4 4H11V3.73244C10.4022 3.38663 10 2.74028 10 2C10 0.895431 10.8954 0 12 0C13.1046 0 14 0.895431 14 2ZM4 6H11H13H20C20.5523 6 21 6.44772 21 7V19C21 19.5523 20.5523 20 20 20H4C3.44772 20 3 19.5523 3 19V7C3 6.44772 3.44772 6 4 6ZM15 11.5C15 10.6716 15.6716 10 16.5 10C17.3284 10 18 10.6716 18 11.5C18 12.3284 17.3284 13 16.5 13C15.6716 13 15 12.3284 15 11.5ZM16.5 8C14.567 8 13 9.567 13 11.5C13 13.433 14.567 15 16.5 15C18.433 15 20 13.433 20 11.5C20 9.567 18.433 8 16.5 8ZM7.5 10C6.67157 10 6 10.6716 6 11.5C6 12.3284 6.67157 13 7.5 13C8.32843 13 9 12.3284 9 11.5C9 10.6716 8.32843 10 7.5 10ZM4 11.5C4 9.567 5.567 8 7.5 8C9.433 8 11 9.567 11 11.5C11 13.433 9.433 15 7.5 15C5.567 15 4 13.433 4 11.5ZM10.8944 16.5528C10.6474 16.0588 10.0468 15.8586 9.55279 16.1056C9.05881 16.3526 8.85858 16.9532 9.10557 17.4472C9.68052 18.5971 10.9822 19 12 19C13.0178 19 14.3195 18.5971 14.8944 17.4472C15.1414 16.9532 14.9412 16.3526 14.4472 16.1056C13.9532 15.8586 13.3526 16.0588 13.1056 16.5528C13.0139 16.7362 12.6488 17 12 17C11.3512 17 10.9861 16.7362 10.8944 16.5528Z"
-            />
-          </svg>
-
-          
-        </label>
-      </div>
+      exit={{ y: -100, opacity: 0 }}
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      className="playerSide">
+      <SideSelection playerSideHandle={playerSideHandle} />
     </motion.div>
-    <motion.div
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ scale: 3 }}
-      transition={{ duration: 1 }}>
-      <div className="gameBody">
-        {winCombinationIndex !== null && (
-          <div
-            className={`winnerLine ${
-              getWinningLineStyle(winCombinationIndex, winner).className
-            }`}
-            style={
-              getWinningLineStyle(winCombinationIndex, winner).style
-            }></div>
-        )}
-        {grid.map((value, index) => (
-          <motion.div
-            key={index}
-            initial={{ scale: 1 }}
-            animate={
-              winningCells.includes(index)
-                ? { scale: getScaleValue() }
-                : { scale: 1 }
-            }
-            transition={{ duration: 0.5, ease: 'easeInOut' }}
-            className={`tacPlace ${value ? '' : 'active'}`}
-            onClick={() => handleClick(index)}>
-            {value && <img src={value === 'X' ? cross : o} alt={value} />}
-          </motion.div>
-        ))}
-      </div>
-    </motion.div>
-  </div>
-    
-    // <motion.div
-    //   key={playerSide}
-    //   initial={{ y: -100, opacity: 0 }}
-    //   animate={{ y: 0, opacity: 1 }}
-    //   exit={{ y: -100, opacity: 0 }}
-    //   transition={{ duration: 0.5, ease: 'easeInOut' }}
-    //   className="playerSide">
-    //   <SideSelection playerSideHandle={playerSideHandle} />
-    // </motion.div>
   );
 }
 
